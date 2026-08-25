55eff01eee726a7a127557cf40234c48c292048c
Your Name <student@nust.na>
2026-08-25 22:07:13 +0200

    Week 1 and Week 2 - C programs

---
+++ week 1/tempCodeRunnerFile.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    char municipality[50];
+    char mayor[50];
+    int population;
+
+    
+    printf("===================================\n");
+    printf(" Municipal Financial Management System\n");
+    printf("===================================\n\n");
+    printf("Welcome to Windhoek Municipality\n\n");
+
+   
+    printf("Enter Municipality Name: ");
+    scanf("%49s", municipality);
+
+    printf("Enter Mayor's Name: ");
+    scanf("%49s", mayor);
+
+    printf("Enter Population: ");
+    scanf("%d", &population);
+
+    printf("\n-----------------------------------\n");
+    printf("        MUNICIPAL REPORT\n");
+    printf("-----------------------------------\n");
+    printf("Municipality : %s\n", municipality);
+    printf("Mayor        : %s\n", mayor);
+    printf("Population   : %d\n", population);
+    printf("-----------------------------------\n");
+
+    return 0;
+}
