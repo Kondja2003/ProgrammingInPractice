6e4e831eee8bd1c461815ada8806263fc7e290f7
Your Name <student@nust.na>
2026-09-21 16:04:32 +0200

    Add week 3 C program

---
+++ week 3/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float basicSalary;
+    float housing;
+    float transport;
+    float tax;
+    float grossSalary;
+    float netSalary;
+
+    printf("Enter basic salary: ");
+    scanf("%f", &basicSalary);
+
+    printf("Enter housing allowance: ");
+    scanf("%f", &housing);
+
+    printf("Enter transport allowance: ");
+    scanf("%f", &transport);
+
+    printf("Enter tax: ");
+    scanf("%f", &tax);
+
+    grossSalary = basicSalary + housing + transport;
+    netSalary   = grossSalary - tax;
+
+    printf("\nGross Salary: %.2f\n", grossSalary);
+    printf("Net Salary: %.2f\n", netSalary);
+
+    return 0;
+}
